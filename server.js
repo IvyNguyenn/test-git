96c7576328c06f455a3d7df21d4099ef3643376c
HoangVy <hoangvynguyen42@gmail.com>
2019-03-23 10:18:59 +0700

    init home-page

--- server.js
+++ server.js
@@ -7,4 +7,8 @@ app.get("/", function(req, res) {
   res.send("<h1>Hello nha!!!</h1>");
 });
 
+app.get("/home", function(req, res) {
+  res.send("<h1>This is home page</h1><hr>");
+});
+
 app.listen(process.env.PORT || PORT);
